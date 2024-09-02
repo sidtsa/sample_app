63a944b86fbfabb38bd920e98fc362a132c4cc32
sid <sidharth.sirish@axtria.com>
2024-09-02 20:01:58 +0530

    Updates

---
+++ assets/NoPage-c27c36ec.js
@@ -0,0 +1,2 @@
+import{a$ as o}from"./vendor-6a2f1f6a.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-c27c36ec.js.map
